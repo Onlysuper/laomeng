94a50e993bde82d299a670ef6840d8537984fe15
apple <apple@apples-MacBook-Pro.local>
2019-04-11 15:27:31 +0800

    语言选择器

--- src/components/m-Language/index.vue
+++ src/components/m-Language/index.vue
@@ -1,18 +1,24 @@
 <template>
-    <Dropdown class="language-box">
+    <Dropdown  @on-click="handleClick" class="language-box">
         <a href="javascript:void(0)">
-            多语言
+            {{lang}}
             <Icon type="ios-arrow-down"></Icon>
         </a>
         <DropdownMenu slot="list">
-            <DropdownItem>
-                <span class="img-box">
-                    <img style="width:100%" :src="require('@/assets/img/english.png')" alt="">
+            <DropdownItem name="chinese">
+                <span class="icon-box">
+                    <Icon :type="lang=='中文简体'?'ios-radio-button-on':'ios-radio-button-off'" />
+                </span>
+                 <span class="img-box">
+                    <img style="width:100%" :src="require('@/assets/img/chinese.png')" alt="">
                 </span>
             </DropdownItem>
-            <DropdownItem>
-                <span class="img-box">
-                    <img style="width:100%" :src="require('@/assets/img/chinese.png')" alt="">
+            <DropdownItem name="english">
+                <span class="icon-box">
+                    <Icon :type="lang=='English'?'ios-radio-button-on':'ios-radio-button-off'"  />
+                </span>
+                 <span class="img-box">
+                    <img style="width:100%" :src="require('@/assets/img/english.png')" alt="">
                 </span>
             </DropdownItem>
             
@@ -24,6 +30,17 @@
 .language-box{
     color:$m-color1;
     margin-left: 10px;
+    display: inline-block;
+    min-width: 100px;
+    .ivu-dropdown-item{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        .icon-box{
+            flex: 0 0 20px;
+            margin-right: 10px;
+        }
+    }
     a{
         color:$m-color1;
         &:hover{
@@ -32,3 +49,34 @@
     }
 }
 </style>
+<script>
+export default {
+    data(){
+        return {
+            lang:"中文简体",
+        }
+    },
+    methods:{
+        handleClick(lang){
+            if(lang=='chinese'){
+                this.lang = "中文简体"
+            }
+             if(lang=='english'){
+                this.lang = "English"
+            }
+        }
+    },
+    watch:{
+        lang(lang){
+            if(lang=="中文简体"){
+
+            }
+            if(lang=="English"){
+                
+            }
+        }
+    }
+    
+}
+</script>
+
