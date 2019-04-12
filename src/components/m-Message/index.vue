2871ae6ad6058cebdb46355bfed355842228a22c
apple <apple@apples-MacBook-Pro.local>
2019-04-12 13:22:55 +0800

    留言板

--- src/components/m-Message/index.vue
+++ src/components/m-Message/index.vue
@@ -2,46 +2,182 @@
 <!-- 留言板 -->
     <div class="m-message-page">
         <div class="m-leave-box">
-            <div>
+            <div class="m-head">
                 <div class="img-box">
-                    <img width="60px" :src="require('@/assets/img/chinese.png')" alt="">
+                    <img  :src="require('@/assets/img/chinese.png')" alt="">
+                </div>
+                <div class="content-box">
+                    一只老萌
+                </div>
+            </div>
+            <div class="m-textarea">
+                <div>
+                    <Input type="textarea" :autosize="{minRows: 5,maxRows: 5}" placeholder="Enter something..."></Input>
+                </div>
+                <div class="m-textarea-foot">
+                    <div>
+
+                    </div>
+                    <div>
+                        <Button  type="primary">确定</Button>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="m-common-list">
-            <Timeline>
-                <TimelineItem color="green">
-                    <Icon type="ios-trophy" slot="dot"></Icon>
-                    <span>发布里程碑版本</span>
-                </TimelineItem>
-                <TimelineItem>发布1.0版本</TimelineItem>
-                <TimelineItem>发布2.0版本</TimelineItem>
-                <TimelineItem>发布3.0版本</TimelineItem>
-            </Timeline>
+        <div class="m-common-container">
+            <div class="m-common-list">
+                <div class="m-img-box">
+                    <img  :src="require('@/assets/img/chinese.png')" alt="">
+                </div>
+                <div class="m-content">
+                    <p class="m-head">
+                        <span class="m-nickname">k.cohen </span>
+                        <time>03-12 11:58</time>
+                    </p>
+                    <div class="m-body">
+                        怎么没有帖出来那张脑图。
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="m-common-container">
+            <div class="m-common-list">
+                <div class="m-img-box">
+                    <img  :src="require('@/assets/img/chinese.png')" alt="">
+                </div>
+                <div class="m-content">
+                    <p class="m-head">
+                        <span class="m-nickname">k.cohen </span>
+                        <time>03-12 11:58</time>
+                    </p>
+                    <div class="m-body">
+                        怎么没有帖出来那张脑图。
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="m-common-container">
+            <div class="m-common-list">
+                <div class="m-img-box">
+                    <img  :src="require('@/assets/img/chinese.png')" alt="">
+                </div>
+                <div class="m-content">
+                    <p class="m-head">
+                        <span class="m-nickname">k.cohen </span>
+                        <time>03-12 11:58</time>
+                    </p>
+                    <div class="m-body">
+                        怎么没有帖出来那张脑图。
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="m-common-container">
+            <div class="m-common-list">
+                <div class="m-img-box">
+                    <img  :src="require('@/assets/img/chinese.png')" alt="">
+                </div>
+                <div class="m-content">
+                    <p class="m-head">
+                        <span class="m-nickname">k.cohen </span>
+                        <time>03-12 11:58</time>
+                    </p>
+                    <div class="m-body">
+                        怎么没有帖出来那张脑图。
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <style lang="scss">
+@import "../../assets/scss/global.scss";
 .m-message-page{
     margin-top: 30px;
     width: 100%;
+    //  background:#fff;
     .m-leave-box{
-        width: 100%;
-        position: relative;
-        height: 60px;
-        z-index: 10;
-        .img-box{
-            width: 42px;
-            height: 42px;
-            position: absolute;
-            left: 9px;
-            top: 22px;
-            display: none;
-            z-index: 11;
-            img{
-                border-radius: 42px;
-                display: block;
+       
+        .m-head{
+            position: relative;
+            width: 100%;
+            height: 60px;
+            z-index: 10;
+            
+            .img-box{
                 width: 42px;
+                height: 42px;
+                position: absolute;
+                left: 9px;
+                top: 30px;
+                z-index: 11;
+                img{
+                    border-radius: 42px;
+                    display: block;
+                    width: 42px;
+                    height: 100%;
+                }
+            }
+            .content-box{
+                position: absolute;
+                top: 15px;
+                right: 0px;
+                left:51px;
+                display: flex;
+                align-items: center;
+                height: 100%;
+                padding-left: 10px;
+            }
+        }
+        .m-textarea{
+            // height: 100px;
+            // border: 1px solid $main-color;
+            border-radius: 10px;
+            textarea{
+                padding-top:15px;
+            }
+            .m-textarea-foot{
+                display: flex;
+                flex-direction: row;
+                padding: 10px 0;
+                justify-content: space-between
+            }
+        }
+        
+    }
+    .m-common-container{
+        padding:10px;
+        width: 100%;
+        background:#fff;
+        margin-top:10px;
+        border-radius: 5px;
+        .m-common-list{
+            display: flex;
+            flex-direction: row;
+            .m-img-box{
+                 width: 42px;
+                height: 42px;
+                img{
+                    border-radius: 42px;
+                    display: block;
+                    width: 42px;
+                    height: 100%;
+                }
+            }
+            .m-content{
+                padding-left: 10px;
+                .m-head{
+                    .m-nickname{
+                        font-size: 16px;
+                    }
+                    time{
+                        margin-left: 5px;
+                        font-size: 10px;
+                    }
+                }
+                .m-body{
+                    padding:10px 0;
+                }
             }
         }
     }
